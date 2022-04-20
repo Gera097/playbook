9652d5a9114658a1b7c53157e5c29fcc9d154bbc
Gera097 <g.sanchezalba@outlook.com>
2022-04-20 17:11:36 -0500

    exercise1 modelado de datos de twitter

---
+++ weekly_mission_2/exercises/exercise1/exercise1_twitter.js
@@ -0,0 +1,22 @@
+
+const user = {
+    user: "Gerardo",
+    username: "Gera097",
+    bio: "...",
+    age: "24",
+    location: "Salamanca, gto",
+    birthday: "september 27",
+    following: 211,
+    followers: "1.5M",
+    joined: "April 2016", 
+    profile_picture: "...",
+    portrait_picture: "...",
+    number_of_tweets: 100,
+    tweets: ["tweet1", "tweet2", "..."],
+    tweets_and_replies: ["tweet1", "tweet2", "..."],
+    media: ["...","...","..."],
+    likes: ["...","...","..."]
+
+}
+
+console.log(user)
