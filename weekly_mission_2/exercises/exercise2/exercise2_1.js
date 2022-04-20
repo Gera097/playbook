d30585d3543b755a6a309cd38f53ef616fae3170
Gera097 <g.sanchezalba@outlook.com>
2022-04-20 17:50:13 -0500

    exercise2_1 2.- Stack de cada explorer usando forEach

--- weekly_mission_2/exercises/exercise2/exercise2_1.js
+++ weekly_mission_2/exercises/exercise2/exercise2_1.js
@@ -65,3 +65,6 @@ const explorers = [
    /* 1.- Imprime el nombre (propiedad name) de cada explorer en la lista, usa FOR EACH */
    explorers.forEach(explorer => console.log(explorer.name))
 
+  /* 2.- Imprime el stack de cada explorer, usa FOR EACH */ 
+  explorers.forEach(explorer => console.log(explorer.stack))
+
