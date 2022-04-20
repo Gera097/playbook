7de77226a039da4e6aec3fd5369d5a2318d60839
Gera097 <g.sanchezalba@outlook.com>
2022-04-20 17:46:33 -0500

    exercise2_1 imprimiendo nombres de los explorers con forEach

---
+++ weekly_mission_2/exercises/exercise2/exercise2_1.js
@@ -0,0 +1,67 @@
+/* Toma la siguiente lista de explorers:  */
+const explorers = [
+    {
+      name: "Explorer 1",
+      exercises_completed: 10,
+      rate: 99,
+      city: "CDMX",
+      stack: [
+        "js",
+        "c#"
+      ],
+      missions: {
+        onboarding: {
+          isFinished: true,
+          exercisesFinished: true
+        },
+        frontend: {
+          isFinished: true,
+          exercisesFinished: true
+        }
+      }
+    },
+    {
+      name: "Explorer 2",
+      exercises_completed: 9,
+      city: "Veracruz",
+      rate: 50,
+      stack: [
+        "js"
+      ],
+      missions: {
+        onboarding: {
+          isFinished: false,
+          exercisesFinished: false
+        },
+        frontend: {
+          isFinished: false,
+          exercisesFinished: false
+        }
+      }
+    },
+    {
+      name: "Explorer 3",
+      exercises_completed: 3,
+      city: "Sonora",
+      rate: 100,
+      stack: [
+        "elixir"
+      ],
+      missions: {
+        onboarding: {
+          isFinished: true,
+          exercisesFinished: true
+        },
+        frontend: {
+          isFinished: false,
+          exercisesFinished: false
+        }
+      }
+    }
+   ]
+
+   /* Sobre esta lista, realiza lo siguiente: */
+
+   /* 1.- Imprime el nombre (propiedad name) de cada explorer en la lista, usa FOR EACH */
+   explorers.forEach(explorer => console.log(explorer.name))
+
