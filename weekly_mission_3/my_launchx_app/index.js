52b01c97c7c4d0e14beabd00629ceb86db078bcb
Gera097 <g.sanchezalba@outlook.com>
2022-04-20 22:31:40 -0500

    3.- Creación de una clase y modularización

---
+++ weekly_mission_3/my_launchx_app/index.js
@@ -0,0 +1,7 @@
+const MissionCommander = require('./app/missionCommander');
+const carlo = new MissionCommander("Carlo")
+const gera = new MissionCommander("Gera")
+const juve = new MissionCommander("Juve")
+console.log(carlo.name)
+console.log(gera.name)
+console.log(juve.name)
